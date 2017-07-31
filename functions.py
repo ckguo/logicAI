9b8650a4f56e3b1151408dc85eab694035a83291
Tiancheng Qin <tianchengq@gmail.com>
2017-07-30 23:50:48 -0700

    Updated functions with better enumeration views

--- functions.py
+++ functions.py
@@ -4,12 +4,12 @@ RED   = "\033[1;31m"
 BLUE  = "\033[1;34m"
 RESET = "\033[0;0m"
 
-def viewEnumeration(enum):
+def viewEnumerationNum(enum):
 	# enum is a 4x6 array of integers (0-23)
-	print('\n\t\tCard 1\tCard 2\tCard 3\tCard 4\tCard 5\tCard 6')
+	print('\tCard 1\tCard 2\tCard 3\tCard 4\tCard 5\tCard 6')
 	for i in range(4):
 		sys.stdout.write(RESET)
-		sys.stdout.write('Player {}:\t'.format(i))
+		sys.stdout.write('Plyr {}:\t'.format(i))
 		for j in range(6):
 			num = enum[i][j]
 			color = RED if num < 12 else BLUE
@@ -17,6 +17,24 @@ def viewEnumeration(enum):
 			sys.stdout.write((str) (num % 12) + '\t')
 		sys.stdout.write('\n')
 	sys.stdout.write(RESET)
+
+def viewEnumerationCard(enum):
+	# enum is a 4x6 array of Card objects:
+	#	- 'rank' is either an int (0-11) or 'Unclear'
+	#	- 'color' is an int (0-1)
+	print('\tCard 1\tCard 2\tCard 3\tCard 4\tCard 5\tCard 6')
+	for i in range(4):
+		sys.stdout.write(RESET)
+		sys.stdout.write('Plyr {}:\t'.format(i))
+		for j in range(6):
+			rank = enum[i][j]['rank']
+			num = (str) (rank % 12) if isinstance(rank, int) else '?'
+			color = RED if enum[i][j]['color'] == 0 else BLUE
+			sys.stdout.write(color)
+			sys.stdout.write(num + '\t')
+		sys.stdout.write('\n')
+	sys.stdout.write(RESET)
+
 
 def cardToNum(card):
 	# card is a dictionary of 'rank' (0-11) and 'color' (0-1)
@@ -34,4 +52,22 @@ def printCard(card):
 
 def numToPrint(num):
 	return printCard(numToCard(num))
+
+"""
+config = [[{'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 1}, {'rank': 3, 'color': 1}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 0}],
+		[{'rank': 'Unclear', 'color': 1}, {'rank': 4, 'color': 1}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 1}],
+		[{'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 0}],
+		[{'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 0}, {'rank': 'Unclear', 'color': 1}, {'rank': 'Unclear', 'color': 0}]]
+
+viewEnumerationCard(config)
+
+arr = []
+for i in range(4):
+	subarr = []
+	for j in range(6):
+		subarr.append(j + 6 * i)
+	arr.append(subarr)
+
+viewEnumerationNum(arr)
+"""
 
